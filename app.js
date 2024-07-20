3addc9a21cb38f66c220bfb6711fc9a107274f54
mahadev <mahadev218das@gmail.com>
2024-07-20 09:44:51 +0530

    v-1.0

---
+++ app.js
@@ -0,0 +1,35 @@
+let input = document.querySelector('input');
+let btn = document.querySelector('button');
+let ul = document.querySelector('ol');
+
+
+btn.addEventListener('click', (e) => {
+    if (input.value.trim() === "") {
+        alert('you didnot enter any text');
+        }
+    else {
+
+        newli();
+        }
+    })
+
+
+
+
+
+function newli() {
+    let li = document.createElement('li');
+    let remvbtn = document.createElement('button');
+    remvbtn.innerText='Remove'
+    remvbtn.style.marginLeft = '10px';
+    remvbtn.style.display = 'inline-block';
+    li.innerText = input.value;
+    li.style.paddingBottom = '10px';
+    console.dir(input);
+    ul.appendChild(li);
+    li.appendChild(remvbtn);
+    input.value=""
+    remvbtn.addEventListener('click', function () {
+        ul.removeChild(li);
+    })
+}
